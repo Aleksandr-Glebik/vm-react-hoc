1bb3cfae4d702da164ecffee96ac6888728e0027
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-03 19:47:23 +0300

    init: start learne hoc

---
+++ react-hoc/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class App extends React.Component {
+
+  render() {
+
+    return (
+      <div>
+        <h1>Hello - learne HOC</h1>
+      </div>
+    )
+  }
+}
+
+export default App
